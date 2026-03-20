67900cc5c74b0e04f75dcce0b05a3edc897c28e2
RicardoLira13 <rycardolira82@gmail.com>
2026-03-20 14:54:12 +0000

    adição de dois navios na diagonal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,6 +6,8 @@ int main(){
     int tabuleiro[10][10];
     int navio_horizontal[3] = {3, 3, 3};
     int navio_vertical[3] = {3, 3, 3};
+    int navio_diagonal1[3] = {3, 3, 3};
+	int navio_diagonal2[3] = {3, 3, 3};
 
     //for que transforma todos os valores do tabuleiro em 0
     for(int i = 0; i < 10; i++){
@@ -22,10 +24,18 @@ int main(){
     tabuleiro[3][5] = navio_vertical[0];
     tabuleiro[4][5] = navio_vertical[1];
     tabuleiro[5][5] = navio_vertical[2];
+    
+    //dois navios na diagonal
+    tabuleiro[0][0] = navio_diagonal1[0];
+    tabuleiro[1][1] = navio_diagonal1[1];
+	tabuleiro[2][2] = navio_diagonal1[2];
 
+	tabuleiro[9][0] = navio_diagonal2[0];
+    tabuleiro[8][1] = navio_diagonal2[1];
+	tabuleiro[7][2] = navio_diagonal2[2];
+	
     //printando o tabuleiro na tela
-
-    printf("***** BATALHA NAVAL *****\n\n");
+    printf("***** TABULEIRO BATALHA NAVAL *****\n\n");
 
     for(int i = 0; i < 10; i++){
         for(int j = 0; j < 10; j++){
@@ -35,6 +45,6 @@ int main(){
     }
 
     printf("\n\n");
-
+    
     return 0;
 }
